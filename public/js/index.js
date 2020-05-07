8e3b06efae0ffff374dbdd7bf13af36d760345d9
Andrew <andrew.j.price@outlook.com>
2020-05-06 18:50:15 -0700

    Display shortened url in style box and add copy text

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
     console.log('js/index.js loaded');
+
     $('#urlForm').submit(function(event) {
       event.preventDefault();
 
@@ -11,8 +12,17 @@ $(document).ready(function() {
         data: data,
       }).done(function(data) {
         $('#result').text(window.location.origin + '/' + data['hash']);
+        $(".newUrl").css("display", "block");
       }).fail(function(xhr, textStatus, errorThrown) {
         console.log(errorThrown);
       });
+    });
+
+    $(".copy").click(function() {
+        var temp = $("<input>");
+        $("body").append(temp);
+        temp.val($("#result").text()).select();
+        document.execCommand("Copy");
+        temp.remove();
     })
 });
